2406f2376a9d8942b39004aafc4744c7b48859a1
Vladislav <v.suvorkin2017@gmail.com>
2025-10-17 19:32:33 +0300

    chore: make aliases

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -8,6 +8,10 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
       '@app': path.resolve(__dirname, './src/app'),
+      '@entities': path.resolve(__dirname, './src/entities'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@shared': path.resolve(__dirname, './src/shared'),
     },
   },
   server: {
