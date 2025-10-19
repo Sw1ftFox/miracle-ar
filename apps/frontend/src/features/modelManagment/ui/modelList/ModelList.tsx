44378233a711b8a190a6bc31c3d2a63752a6423f
Vladislav <v.suvorkin2017@gmail.com>
2025-10-19 19:48:28 +0300

    feat: add model type and mock data to display

--- apps/frontend/src/features/modelManagment/ui/modelList/ModelList.tsx
+++ apps/frontend/src/features/modelManagment/ui/modelList/ModelList.tsx
@@ -1,12 +1,23 @@
 import ModelItem from "../modelItem/ModelItem";
+import type { ModelType } from "../../types";
 import styles from "./modelList.module.css";
+
+const mockModels: ModelType[] = [
+  {
+    id: "polonsky",
+    name: "Полонский",
+    previewUrl: "/mock-images/polonsky.jpg",
+    description: "3D модель персонажа",
+    isCurrent: true,
+  },
+];
 
 const ModelList = () => {
   return (
     <div className={styles.models__gallery}>
-      <ModelItem />
-      <ModelItem />
-      <ModelItem />
+      {mockModels.map((model) => (
+        <ModelItem model={model} />
+      ))}
     </div>
   );
 };
