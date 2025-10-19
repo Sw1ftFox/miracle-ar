76e48849ca749726d2f6e3f75e4b5d868ac03747
Vladislav <v.suvorkin2017@gmail.com>
2025-10-19 19:48:28 +0300

    feat: add model type and mock data to display

--- apps/frontend/src/features/modelManagment/ui/modelItem/ModelItem.tsx
+++ apps/frontend/src/features/modelManagment/ui/modelItem/ModelItem.tsx
@@ -1,23 +1,28 @@
-import styles from "./modelItem.module.css"
+import type { ModelType } from "../../types";
+import styles from "./modelItem.module.css";
 
-const ModelItem = () => {
+type Props = {
+  model: ModelType;
+};
+
+const ModelItem = ({ model }: Props) => {
   return (
     <div className={styles.model__card}>
       <div className={styles.image__container}>
         <img
-          src="/api/files/images/${image}"
-          alt="${modelName}"
+          src={model.previewUrl}
+          alt={model.name}
           className={styles.model__image}
         />
         <div className={styles.no__image}>Картинка отсутствует</div>
       </div>
       <div className={styles.model__content}>
-        <h3 className={styles.model__title}>modelName</h3>
-        <p className={styles.model__description}>description.content</p>
+        <h3 className={styles.model__title}>{model.name}</h3>
+        <p className={styles.model__description}>{model.description}</p>
         <p className={styles.no__description}>Описание отсутствует</p>
         <p className={styles.pattern__info}></p>
         <p className={styles.pattern__warning}>⚠ Паттерн отсутствует</p>
-        <button className={styles.view__btn} data-model="${model}">
+        <button className={styles.view__btn} data-model={model}>
           Просмотр в AR
         </button>
       </div>
