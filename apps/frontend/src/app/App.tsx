1a722fe22f0160338b78268378f6c5c46f56045e
Vladislav <v.suvorkin2017@gmail.com>
2025-10-19 16:07:13 +0300

    feat: fill the page components with template code and configure routing

--- apps/frontend/src/app/App.tsx
+++ apps/frontend/src/app/App.tsx
@@ -1,9 +1,25 @@
+import {
+  AdminPage,
+  AuthPage,
+  Error404Page,
+  ModelPage,
+  ModelsPage,
+} from "@/pages";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
 function App() {
   return (
-    <>
-      
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<ModelsPage />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/models" element={<ModelsPage />} />
+        <Route path="/models/:modelId" element={<ModelPage />} />
+        <Route path="*" element={<Error404Page />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ apps/frontend/src/pages/modelsPage/ModelsPage.tsx
@@ -0,0 +1,11 @@
+import { ModelList } from "@/features/modelManagment/ui";
+
+const ModelsPage = () => {
+  return (
+    <div>
+      <ModelList />
+    </div>
+  );
+};
+
+export default ModelsPage;
