c22bbfddff79a1cdbacd521f360dec41dbb39043
Vladislav <v.suvorkin2017@gmail.com>
2025-10-19 16:07:13 +0300

    feat: fill the page components with template code and configure routing

---
+++ apps/frontend/src/features/modelManagment/ui/modelList/ModelList.tsx
@@ -0,0 +1,13 @@
+import ModelItem from "../modelItem/ModelItem";
+
+const ModelList = () => {
+  return (
+    <div>
+      <ModelItem />
+      <ModelItem />
+      <ModelItem />
+    </div>
+  );
+};
+
+export default ModelList;
