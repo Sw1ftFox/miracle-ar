1197bf1033e8a9108287b9134f8d6d17c26fa4e5
Vladislav <v.suvorkin2017@gmail.com>
2025-10-26 18:06:39 +0300

    feat: write slice for models

--- apps/frontend/src/features/modelManagment/types.ts
+++ apps/frontend/src/features/modelManagment/types.ts
@@ -1,18 +1,16 @@
 export type ModelType = {
   id: string; // "Полонский" (имя без расширения)
   name: string; // "Полонский.glb"
-  // modelUrl: string; // "/api/files/models/Полонский.glb"
-  // patternUrl: string; // "/api/files/patterns/Полонский.patt"
+  modelUrl: string; // "/api/files/models/Полонский.glb"
+  patternUrl: string; // "/api/files/patterns/Полонский.patt"
   soundUrl?: string; // "/api/files/sounds/Полонский.mp3" (если есть)
   previewUrl?: string; // "/api/files/images/Полонский.jpg" (если есть)
   description?: string; // Текст из "/api/models/description/Полонский.txt"
-  isCurrent: boolean; // Сравнить с /api/models/current
 }
 
 export type AppState = {
   models: ModelType[];
-  currentModel: string | null;
-  isAdmin: boolean;
   isLoading: boolean;
-  error: string | null;
+  isError: boolean;
+  errorMessage: string | undefined;
 }
