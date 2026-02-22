71f2411239e098414597cfe6b6fd21df9c317b1f
Vladislav <v.suvorkin2017@gmail.com>
2026-02-22 19:42:36 +0300

    chore: delete debug info

--- apps/frontend/src/features/auth/authSlice.ts
+++ apps/frontend/src/features/auth/authSlice.ts
@@ -3,7 +3,6 @@ import type { authType } from "./types";
 import type { RootState } from "@/app/store";
 
 const API_BASE = import.meta.env.VITE_API_URL;
-console.log('API_BASE =', API_BASE);
 
 const initialState: authType = {
   isAuth: false,
