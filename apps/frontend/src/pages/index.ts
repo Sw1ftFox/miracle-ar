42cd2e0b58949541b80108927982565128a9a426
Vladislav <v.suvorkin2017@gmail.com>
2026-03-11 12:33:38 +0300

    refactor: replace ARViewerPage ARViewerPageHTML component

--- apps/frontend/src/pages/index.ts
+++ apps/frontend/src/pages/index.ts
@@ -1,8 +1,9 @@
 import ModelsPage from "./modelsPage/ModelsPage";
 import ARViewerPage from "./ARViewerPage/ARViewerPage";
+import ARViewerPageHTML from "./ARViewerPageHTML/ARViewerPageHTML";
 import ModelViewerPage from "./ModelViewerPage/ModelViewerPage";
 import AuthPage from "./authPage/AuthPage";
 import AdminPage from "./adminPage/AdminPage";
 import Error404Page from "./Error404Page/Error404Page";
 
-export { ModelsPage, ARViewerPage, ModelViewerPage, AuthPage, AdminPage, Error404Page };
+export { ModelsPage, ARViewerPage, ARViewerPageHTML, ModelViewerPage, AuthPage, AdminPage, Error404Page };
