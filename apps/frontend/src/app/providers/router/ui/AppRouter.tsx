984e1eb4b7c837bc28bc4c1722c899bc68e2cec4
Vladislav <v.suvorkin2017@gmail.com>
2026-04-22 14:27:52 +0300

    fix: delete div block from AppRouter

--- apps/frontend/src/app/providers/router/ui/AppRouter.tsx
+++ apps/frontend/src/app/providers/router/ui/AppRouter.tsx
@@ -8,11 +8,7 @@ export const AppRouter = () => {
     <Suspense fallback={<PageLoader />}>
       <Routes>
         {Object.values(routeConfig).map(({ path, element }) => (
-          <Route
-            key={path}
-            path={path}
-            element={<div className="page-wrapper">{element}</div>}
-          />
+          <Route key={path} path={path} element={<>{element}</>} />
         ))}
       </Routes>
     </Suspense>
