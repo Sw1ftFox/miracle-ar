88fac5c00083ab6d4dd73dbfeeee1f0544b5302a
Vladislav <v.suvorkin2017@gmail.com>
2026-04-22 13:27:31 +0300

    fix: fix relative position ARViewerPageHTML.module.css

--- apps/frontend/src/pages/index.ts
+++
@@ -1,7 +0,0 @@
-export { AuthPage } from './AuthPage';
-export { AdminPage } from './AdminPage';
-export { ModelViewerPage } from './ModelViewerPage';
-export { ARViewerPageHTML } from './ARViewerPageHTML';
-export { ARViewerPage } from './ARViewerPage';
-export { ModelsPage } from "./ModelsPage";
-export { NotFoundPage } from "./NotFoundPage";
