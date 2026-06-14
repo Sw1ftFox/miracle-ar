9d583574e431dac03b0e2806b73624486a0e0f25
Vladislav <v.suvorkin2017@gmail.com>
2026-06-14 22:50:55 +0300

    chore: return cors check

--- apps/backend/src/main/java/com/miraclear/viewing/config/SecurityConfig.java
+++ apps/backend/src/main/java/com/miraclear/viewing/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .cors(cors -> cors.disable())
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
